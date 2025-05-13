484e0310486d183d50e53bc6118ab6bf6383c1b8
Sutharsika Kumar <sutharsikakumar@Sutharsikas-MacBook-Air-2612.local>
2025-05-13 17:01:01 -0400

    idk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Inter } from 'next/font/google';
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-});
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
